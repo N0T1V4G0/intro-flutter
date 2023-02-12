349e1065548c0fddfc0cf0637c40221b8569fb02
Lucas Barbosa <lukeb.trybe@gmail.com>
2023-02-12 11:23:00 -0300

    add stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,48 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var questions = ['What\'s your favorite color?', 'What\'s your pet name?'];
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+    print('Question answered');
+  }
+
   Widget build(BuildContext ctx) {
     return MaterialApp(
-      home: Text('Hello world!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Whatever'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(
+              child: Text('Button 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Button 2'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Button 3'),
+              onPressed: answerQuestion,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
